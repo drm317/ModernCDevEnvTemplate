e08a6f6ca9e238b78a4dfa85f2afcb905f9e9d3d
Daniel <drm3107@gmail.com>
2025-08-31 22:06:05 +0100

    Removed hello world example

--- src/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "math_utils.h"
-
-int main() {
-    int a = 5, b = 3;
-    printf("Addition: %d + %d = %d\n", a, b, add(a, b));
-    printf("Subtraction: %d - %d = %d\n", a, b, subtract(a, b));
-    return 0;
-}
